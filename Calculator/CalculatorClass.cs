b4db4ea7fb0fda08671a7ae372029557e8dc3e66
EgorSalahov <egorsalahov2@gmail.com>
2025-03-13 23:17:51 +0300

    Добавьте файлы проекта.

---
+++ Calculator/CalculatorClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class CalculatorClass
+    {
+        private IOperation _operation;
+
+        public void SetOperation(IOperation operation) { _operation = operation; }
+
+        public void DoCalulate(int i, int j)
+        {
+            _operation.DoOperation(i,j);
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using Calculator;
+using Calculator.Patterns;
+
+string s = Console.ReadLine();
+string[] split = s.Split(" ");
+int i = Convert.ToInt32(split[0]);
+int j = Convert.ToInt32(split[2]);
+
+CalculatorClass calculator = new CalculatorClass();
+
+switch (split[1].ToString())
+{
+    case "+": calculator.SetOperation(new Plus()); calculator.DoCalulate(i, j); break;
+    case "-": calculator.SetOperation(new Minus()); calculator.DoCalulate(i, j); break;
+    case "*": calculator.SetOperation(new Multiply()); calculator.DoCalulate(i, j); break;
+    case "/": calculator.SetOperation(new Divide()); calculator.DoCalulate(i, j); break;
+    default: Console.WriteLine("Такой операции нет"); break;
+}
